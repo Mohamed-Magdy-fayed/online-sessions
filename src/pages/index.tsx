dcdf106503aefb5190e167843b73b019a3f2dfef
Mohamed-Magdy-fayed <mohamed.ahmed.sc2@gmail.com>
2023-07-25 16:17:12 +0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Session Viewer</title>
+        <meta name="description" content="Online Sessions Viewer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="">Hi</main>
+    </>
+  );
+}
